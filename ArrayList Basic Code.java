0a976bd56b1c1dbb6c1d9db707023f95c5d8f068
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-26 16:19:00 +0600

    Create ArrayList Basic Code.java

---
+++ ArrayList Basic Code.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main { 
+  public static void main(String[] args) { 
+    ArrayList<String> cars = new ArrayList<String>();
+    cars.add("Jakaria");
+    cars.add("Ansari");
+    cars.add("Hafiz");
+    cars.add("Reduan");
+    System.out.println(cars);
+  } 
+}
