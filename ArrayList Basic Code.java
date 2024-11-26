85cd5f919ae363ad04b69e2ed1992c46a9f544e2
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-26 16:43:55 +0600

    Update ArrayList Basic Code.java

--- ArrayList Basic Code.java
+++ ArrayList Basic Code.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class Main { 
   public static void main(String[] args) { 
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Jakaria");
-    cars.add("Ansari");
-    cars.add("Hafiz");
-    cars.add("Reduan");
-    System.out.println(cars);
+    ArrayList<String> name = new ArrayList<String>();
+    name.add("Jakaria");
+    name.add("Ansari");
+    name.add("Hafiz");
+    name.add("Reduan");
+    System.out.println(name);
   } 
 }
