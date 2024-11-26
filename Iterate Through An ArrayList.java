29c02ec927893f72b4581cad628fc1d70d7d1158
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-11-26 17:29:13 +0600

    Update and rename Iterate through an ArrayList.java to Iterate Through An ArrayList.java

--- Iterate Through An ArrayList.java
+++ Iterate Through An ArrayList.java
@@ -7,12 +7,12 @@ class Main {
     languages.add("C++");
     languages.add("C#");
     languages.add("Java");
-    languages.add("JavaScript");
-    languages.add("TypeScript");
     languages.add("PHP");
     languages.add("Python");
-    System.out.println("ArrayList of Programming Language: "+"\n" + languages);
+    languages.add("JavaScript");
+    languages.add("TypeScript");
     
+    System.out.println("ArrayList of Programming Language: "+"\n" + languages);
     System.out.println("Iterate through an ArrayList: "+"\n");
     for(String i: languages){
         System.out.println(i);
